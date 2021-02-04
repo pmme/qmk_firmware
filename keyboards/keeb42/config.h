4352c618c5f1785493f3a59172715f000fcba00e
pmme <gpaulmcs@gmail.com>
2021-02-05 12:09:42 +1300

    [Keyboard] Add keeb42 keyboard

---
+++ keyboards/keeb42/config.h
@@ -0,0 +1,34 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define COMBO_COUNT 2
+#define COMBO_TERM 100
+
+#include "config_common.h"
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x6062
+#define DEVICE_VER      0x0002
+#define MANUFACTURER    ntope
+#define PRODUCT         keeb42
+#define DESCRIPTION     split keyboard inspired by gergo from gboards.ca
+
+#define MATRIX_ROWS 8
+#define MATRIX_COLS 6
+
+#define MATRIX_ROW_PINS { D7, E6, B4, B5 }
+#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3 }
+#define UNUSED_PINS
+
+#define ENCODERS_PAD_A { D4 }
+#define ENCODERS_PAD_B { C6 }
+#define ENCODER_RESOLUTION 4
+
+#define DIODE_DIRECTION COL2ROW
+
+#define SOFT_SERIAL_PIN D0
+#define SPLIT_USB_DETECT
+#define MASTER_LEFT
+
+#define RGBLED_NUM 0
+
+#endif
