80ee0951e5271ad7ef3569bf3751563704db9b9c
pmme <gpaulmcs@gmail.com>
2021-03-01 15:21:14 +1300

    Swap CTRL & LSHIFT and add CAPS as tap on LCTRL.

--- keyboards/keeb42/keymaps/pmme/keymap.c
+++ keyboards/keeb42/keymaps/pmme/keymap.c
@@ -35,28 +35,29 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Base layer
-   *  CTTB = Control (hold), Tab (tap)
+   *  CTCAP = Control (hold), CAPS (tap)
+   *  SHTAB = Left Shift (hold), Tab (tap)
    *  WINDEL = Win (hold), Delete (tap)
-   *  SHENT = Shift (hold), Enter (tap)
+   *  SHENT = Right Shift (hold), Enter (tap)
    *  SYMENT = Symbols (hold), Enter (tap)
    *  NUMSPC = Numbers (hold), Space (tap)
    *  ARWSPC = Arrows (hold), Space (tap)
    * ,-----------------------------------------.              ,-----------------------------------------.
    * |  ESC |  Q   |  W   |  E   |  R   |  T   |              |   Y  |   U  |  I   |  O   |  P   | BSPC |
    * |------+------+------+------+------+------|              |------+------+------+------+------+------|
-   * | CTTB |  A   |  S   |  D  tab F   |  G   |              |   H  |  J   |  K   |  L  ent ; : | ' "  |
+   * |SHTAB |  A   |  S   |  D  tab F   |  G   |              |   H  |  J   |  K   |  L  ent ; : | ' "  |
    * |------+------+------+------+------+------|              |------+------+------+------+------+------|
-   * | SHFT |  Z   |  X   |  C   |  V   |  B   |              |   N  |  M   |  ,   |  . > |  / ? |SHENT |
+   * |CTCAP |  Z   |  X   |  C   |  V   |  B   |              |   N  |  M   |  ,   |  . > |  / ? |SHENT |
    * `------+------+------+------+------+------'              `------+------+------+------+------+------'
    *              .------.   .------.   .------. .----..----. .------.   .------.   .------.
    *              |WINDEL|   |SYMENT|   |NUMSPC| | TG || TG | |ARWSPC|   | ALT  |   | WIN  |
    *              '------'   '------'   '------' '----''----' '------'   '------'   '------'
    */
   [BASE] = LAYOUT_keeb42(
-      KC_ESC,      KC_Q,      KC_W,  KC_E,   KC_R,   KC_T,                  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-      MT(MOD_LCTL, KC_TAB),   KC_A,  KC_S,   KC_D,   KC_F,                  KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-      KC_LSFT,     KC_Z,      KC_X,  KC_C,   KC_V,   KC_B,                  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_RSFT, KC_ENT),
-       MT(MOD_LGUI, KC_DEL), LT(SYMB, KC_ENT), LT(NUMB, KC_SPC), TG_LENC,   TG_RENC, LT(ARRW, KC_SPC), KC_RALT, KC_RGUI
+      KC_ESC,               KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+      MT(MOD_LSFT,KC_TAB),  KC_A,    KC_S,    KC_D,    KC_F,     KC_G,       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+      MT(MOD_LCTL,KC_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_RSFT,KC_ENT),
+         MT(MOD_LGUI,KC_DEL), LT(SYMB,KC_ENT), LT(NUMB,KC_SPC), TG_LENC,    TG_RENC, LT(ARRW,KC_SPC), KC_RALT, KC_RGUI
       ),
 
   /* Symbols layer
